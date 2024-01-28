321f21b14fe11f47188d8cfcd96f4e39da59614b
Nikita Murenkii <nikitamrky@gmail.com>
2024-01-28 21:47:20 +0300

    Added reminder to compose unique areas list

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import random
 
 
 def main():
-
     # Define all dungeon meta data variables except themes
     size = random.choice(data.SIZES)
     area_limit = get_area_limit_number(size)
@@ -18,18 +17,18 @@ def main():
 
     # Generate themes
     themes = []
-    for i in range (themes_number):
+    for i in range(themes_number):
         countdown = get_themes_number(size)
         themes.append(get_theme() + ": %s" % countdown)
 
     # Construct response string
     s = ("Dungeon #1:\n" \
-        "Size: %s\n" \
-        "Area limit: %i\n" \
-        "Builded by %s\n" \
-        "Function: %s\n" \
-        "Ruined by: %s\n" \
-        "Themes & countdowns:" \
+         "Size: %s\n" \
+         "Area limit: %i\n" \
+         "Builded by %s\n" \
+         "Function: %s\n" \
+         "Ruined by: %s\n" \
+         "Themes & countdowns:" \
          % (size, area_limit, builder, function, ruination))
 
     # Append string with themes
@@ -43,14 +42,20 @@ def main():
     while True:
         proceed = input("Do you want to proceed with this dungeon? [y/n]: ")
         if "n" in proceed.lower():
-            quit(1)
+            quit(1)  # TODO: chatbot will offer another dungeon
         elif not "y" in proceed.lower():
             pass
         else:
             break
 
+    # Reminder to compose unique areas list
+    print("Write down %i unique areas aligning with dungeon themes. \n"
+          "Roll or choose one of them every time you need to present a unique area.\n"
+          % (int(area_limit / 2)))
+
+    # TODO: offer some options to exclude (e.g. water-going creatures) via button menu
+
     # Generate new random area
-    pass
 
 
 if __name__ == "__main__":
