c1f64d07dd16e5aca62a982cd25652d92ad8b7de
Nikita Murenkii <nikitamrky@gmail.com>
2025-01-19 18:25:13 +0100

    Start command handler added

--- telegram/handlers.py
+++ telegram/handlers.py
@@ -1,12 +1,21 @@
+from telegram.messages import texts, keyboards
+
 from aiogram import Router, types
 from aiogram.filters import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 # Создаем роутер
 r = Router()
 
 
-# Хэндлер на команду /start
+# /start: выбрать размер подземелья
 @r.message(Command("start"))
 async def cmd_test1(message: types.Message):
-    await message.reply("Test")
+    await message.answer(
+        texts.choose_size.get_text_ru(),
+        reply_markup=keyboards.choose_size.get_keyboard("ru"),
+    )
+
+
+# @r.message()
